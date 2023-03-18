409a0c8a90017fb048d370990ba6ceac36935fbd
Roope Virtanen <roopekalle16@gmail.com>
2023-03-18 02:46:26 +0200

    Imported XR Interaction Toolkit, player can pickup boxes, boxes have physics, controller are shown in each hand, player can move

---
+++ Assets/Scripts/Blaster.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Blaster : MonoBehaviour
+{
+    [SerializeField] Rigidbody projectilePrefab;
+    [SerializeField] Transform spawnPoint;
+}
